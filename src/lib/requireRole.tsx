65bda4811d2ed128c9e3328ff6f4febaac2fb43e
TAT610 <trananhtuyet0610@gmail.com>
2025-08-12 19:15:12 +0700

    .

--- src/lib/requireRole.tsx
+++ src/lib/requireRole.tsx
@@ -3,11 +3,53 @@ import { useRouter } from "next/navigation";
 import { useAuth } from "@/lib/AuthContext";
 import LoadingScreen from "@/components/common/LoadingScreen";
 
-export function withRoleProtection<P>(
+// export function withRoleProtection<P>(
+//   WrappedComponent: React.ComponentType<P>,
+//   allowedRoles: Array<0 | 1 | 2 | 3 | 4 | 5>
+// ) {
+//   const ProtectedComponent: React.FC<React.PropsWithChildren<P>> = (props) => {
+//     const { user, loading } = useAuth();
+//     const router = useRouter();
+
+//     const isAllowedRole = (role: number): role is 0 | 1 | 2 | 3 | 4 | 5 =>
+//       [0, 1, 2, 3, 4, 5].includes(role);
+
+//     useEffect(() => {
+//       if (
+//         !loading &&
+//         (!user ||
+//           !isAllowedRole(user.role) ||
+//           !allowedRoles.includes(user.role))
+//       ) {
+//         router.push("/authentication/login");
+//       }
+//     }, [user, loading, router]);
+
+//     if (
+//       loading ||
+//       !user ||
+//       !isAllowedRole(user.role) ||
+//       !allowedRoles.includes(user.role)
+//     ) {
+//       // Đợi redirect hoặc đang loading
+//       return (
+//         <div className="w-full">
+//           <LoadingScreen />
+//         </div>
+//       );
+//     }
+
+//     return <WrappedComponent {...props} />;
+//   };
+
+//   return ProtectedComponent;
+// }
+
+export function withRoleProtection<P extends object>(
   WrappedComponent: React.ComponentType<P>,
   allowedRoles: Array<0 | 1 | 2 | 3 | 4 | 5>
 ) {
-  const ProtectedComponent: React.FC<React.PropsWithChildren<P>> = (props) => {
+  const ProtectedComponent = (props: P) => {
     const { user, loading } = useAuth();
     const router = useRouter();
 
@@ -31,7 +73,6 @@ export function withRoleProtection<P>(
       !isAllowedRole(user.role) ||
       !allowedRoles.includes(user.role)
     ) {
-      // Đợi redirect hoặc đang loading
       return (
         <div className="w-full">
           <LoadingScreen />
@@ -41,6 +82,10 @@ export function withRoleProtection<P>(
 
     return <WrappedComponent {...props} />;
   };
+
+  ProtectedComponent.displayName = `withRoleProtection(${
+    WrappedComponent.displayName || WrappedComponent.name || "Component"
+  })`;
 
   return ProtectedComponent;
 }
