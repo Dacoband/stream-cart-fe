9c2d92aa95f052c2a15c5eeb7e1f56d00b3a2844
TAT610 <trananhtuyet0610@gmail.com>
2025-09-02 17:58:54 +0700

    Update layout.tsx

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -15,7 +15,9 @@ export default async function LayoutCustomer({
       <div>
         <div className="min-h-screen bg-[#F5F5F5]">
           <div className="fixed top-0 left-0 right-0  h-[8vh] w-full z-50">
-            <Navigation />
+            <Suspense fallback={null}>
+              <Navigation />
+            </Suspense>
           </div>
           <div className="flex flex-1 w-full justify-center">
             <div className="w-full mt-[8vh]">
