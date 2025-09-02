7593420dd370edc8935aa0ee653145738e868ca5
TAT610 <trananhtuyet0610@gmail.com>
2025-09-02 14:24:07 +0700

    fix deployce

--- src/app/(root)/home/page.tsx
+++ src/app/(root)/home/page.tsx
@@ -1,38 +1,10 @@
-"use client";
-
-import { useState } from "react";
-import ChatBot from "../components/ChatBot";
-import ChatWithShop from "../components/ChatWithShop";
-import Advertisement from "./components/advertisement";
-import CategoryComponent from "./components/Category";
-import FlashSale from "./components/FlashSale";
-import LiveStreaming from "./components/LiveStreaming";
-import RecommendedProducts from "./components/RecommendedProducts";
+import { Suspense } from "react";
+import HomeInner from "./components/HomeInner";
 
 export default function Home() {
-  const [openBot, setOpenBot] = useState(false);
-  const [openShop, setOpenShop] = useState(false);
-
-  // Khi mở cái này thì đóng cái kia
-  const handleOpenBot = () => {
-    setOpenBot((prev) => !prev);
-    if (!openBot) setOpenShop(false);
-  };
-  const handleOpenShop = () => {
-    setOpenShop((prev) => !prev);
-    if (!openShop) setOpenBot(false);
-  };
-
   return (
-    <div className="flex flex-col w-[80%] mx-auto gap-5 mb-16 mt-2">
-      <Advertisement />
-      <CategoryComponent />
-      <LiveStreaming />
-      <FlashSale />
-      <RecommendedProducts />
-
-      <ChatBot open={openBot} setOpen={handleOpenBot} />
-      <ChatWithShop open={openShop} setOpen={handleOpenShop} shopId="" />
-    </div>
+    <Suspense>
+      <HomeInner />
+    </Suspense>
   );
 }
--- src/app/customer/cart/page.tsx
+++ src/app/customer/cart/page.tsx
@@ -1,50 +1,10 @@
-"use client";
+import { Suspense } from "react";
+import CartPageInner from "./components/CartPageInner";
 
-import React, { useEffect, useState } from "react";
-import { getCart } from "@/services/api/cart/cart";
-import { Cart } from "@/types/Cart/Cart";
-import { withRoleProtection } from "@/lib/requireRole";
-import LoadingScreen from "@/components/common/LoadingScreen";
-import EmptyCart from "./components/EmptyCart";
-import ShoppingCart from "./components/ShoppingCart";
-
-function CartPage() {
-  const [cart, setCart] = useState<Cart | null>(null);
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    const fetchCart = async () => {
-      try {
-        const res = await getCart();
-        setCart(res);
-        console.log("Fetch cart sucssess", res);
-      } catch (err) {
-        console.error("Fetch error api Cart", err);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchCart();
-  }, []);
-
-  if (loading)
-    return (
-      <div>
-        <LoadingScreen />
-      </div>
-    );
-  if (!cart?.cartItemByShop || cart.cartItemByShop.length === 0)
-    return (
-      <div className="flex w-[70%] mx-auto h-full">
-        <EmptyCart />
-      </div>
-    );
-
+export default function Page() {
   return (
-    <div className="w-[70%] mx-auto mt-10">
-      <ShoppingCart cart={cart} />
-    </div>
+    <Suspense>
+      <CartPageInner />
+    </Suspense>
   );
 }
-
-export default withRoleProtection(CartPage, [1]);
